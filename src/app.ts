6a8ad943e5675af208380383e7e9e2a8a198f868
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-07-27 21:54:17 +0600

    prettier setup done

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response, urlencoded } from "express";
-import cors from "cors";
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 //use cors
-app.use(cors());
+app.use(cors())
 
 //use express parser
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
