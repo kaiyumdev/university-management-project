280cbd3dc85d9f7a123d56877d4997da6a6efa35
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-06 23:54:03 +0600

    [DONE]updated academic semester sevice,controller & route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './middlewares/globalErrorHandler';
 import { UserRoutes } from './app/modules/users/user.route';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
 const app: Application = express();
 
 // use cors
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // console.log(process.env);
 
 app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 
 // Testing routes
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import validateRequest from '../../../middlewares/validateRequest';
 import { AcademicSemesterValidation } from './academicSemester.validation';
+import { AcademicSemesterController } from './academicSemester.controller';
 
 const router = express.Router();
 
 router.post(
-  '/create-user',
+  '/create-semester',
   validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  AcademicSemesterController.createSemester,
   //   UserController.createUser,
 );
 
-export const UserRoutes = router;
+export const AcademicSemesterRoutes = router;
