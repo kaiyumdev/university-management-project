f8daa8cfdefa79007a8a3b621487c078c3310384
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-07 21:42:30 +0600

    implement next function

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,19 +1,22 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AcademicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
 // import { z } from 'zod'
 
-const createSemester = catchAsync(async (req: Request, res: Response) => {
-  const { ...academicSemesterData } = req.body;
-  const result = await AcademicSemesterService.createSemester(
-    academicSemesterData,
-  );
-  res.status(200).json({
-    success: true,
-    message: 'Academic semester is created successfully',
-    data: result,
-  });
-});
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ...academicSemesterData } = req.body;
+    const result = await AcademicSemesterService.createSemester(
+      academicSemesterData,
+    );
+    next();
+    res.status(200).json({
+      success: true,
+      message: 'Academic semester is created successfully',
+      data: result,
+    });
+  },
+);
 export const AcademicSemesterController = {
   createSemester,
 };
--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,12 +1,13 @@
-import { Request, RequestHandler, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
 // import { z } from 'zod'
 
 const createUser: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body;
     const result = await UserService.createUser(user);
+    next();
     res.status(200).json({
       success: true,
       message: 'User created successfully',
