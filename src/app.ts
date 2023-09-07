540ab5738220bd742570d02b45f9e5428a8bb30a
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-07 21:23:39 +0600

    [DONE]optimize application routes & controller

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './middlewares/globalErrorHandler';
-import { UserRoutes } from './app/modules/users/user.route';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import routes from './app/routes';
 const app: Application = express();
 
 // use cors
@@ -16,8 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 // Application routes
 // console.log(process.env);
 
-app.use('/api/v1/users', UserRoutes);
-app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+// app.use('/api/v1/users', UserRoutes);
+// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+app.use('/api/v1/', routes);
 
 // Testing routes
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
