aa5dfdcb8dc5c6c14964daa6f6d30eef3f7abcf4
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-07 15:32:06 +0600

    [DONE]semester code validation

--- src/app/modules/academicSemester/academicSemesterModel.ts
+++ src/app/modules/academicSemester/academicSemesterModel.ts
@@ -9,7 +9,7 @@ import {
   academicSemesterTitles,
 } from './academicSemester.constant';
 import ApiError from '../../../errors/ApiError';
-import status from 'http-status';
+import httpStatus from 'http-status';
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -47,7 +47,10 @@ academicSemesterSchema.pre('save', async function (next) {
     year: this.year,
   });
   if (isExist) {
-    throw new ApiError(status.CONFLICT, 'Academic semester is already exists!');
+    throw new ApiError(
+      httpStatus.CONFLICT,
+      'Academic semester is already exists!',
+    );
   }
   next();
 });
