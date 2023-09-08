7618be59acda129ef848c707d1af008cd88f46ca
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-08 22:56:59 +0600

    updated sendResponse file

--- src/shared/catchAsync.ts
+++ src/shared/catchAsync.ts
@@ -10,14 +10,4 @@ const catchAsync = (fn: RequestHandler) => {
   };
 };
 
-// const catchAsync = (fn: RequestHandler) => {
-//   return async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       fn(req, res, next);
-//     } catch (error) {
-//       next(error);
-//     }
-//   };
-// };
-
 export default catchAsync;
