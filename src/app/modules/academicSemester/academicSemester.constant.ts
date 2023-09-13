00e2ffca8261844a2b0ce9b15083945153fe89ce
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-13 19:27:10 +0600

    updated academicSemester.constant

--- src/app/modules/academicSemester/academicSemester.constant.ts
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -10,6 +10,27 @@ export const academicSemesterTitles: IAcademicSemesterTitles[] = [
   'Fall',
 ];
 
+// export const academicSemesterCodes: IAcademicSemesterCodes[] = [
+//   '01',
+//   '02',
+//   '03',
+// ];
+
+// export const academicSemesterMonths: IAcademicSemesterMonths[] = [
+//   'January',
+//   'February',
+//   'March',
+//   'April',
+//   'May',
+//   'June',
+//   'July',
+//   'August',
+//   'September',
+//   'October',
+//   'November',
+//   'December',
+// ];
+
 export const academicSemesterCodes: IAcademicSemesterCodes[] = [
   '01',
   '02',
