de556a65a69e9e84ef9e63f77c172688eed9ea0e
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-24 01:51:12 +0600

    create single-product successfully

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -6,35 +6,87 @@ import httpStatus from 'http-status';
 import pick from '../../../shared/pick';
 import { paginationFields } from '../../../constants/pagination';
 import { IAcademicSemester } from './academicSemester.interface';
+import { academicSemesterFilterableFields } from './academicSemester.constant';
 // import { z } from 'zod'
+
+// const createSemester = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const { ...academicSemesterData } = req.body;
+//     const result = await AcademicSemesterService.createSemester(
+//       academicSemesterData,
+//     );
+//     // res.status(200).json({
+//     //   success: true,
+//     //   message: 'Academic semester is created successfully',
+//     //   data: result,
+//     // });
+//     sendResponse(res, {
+//       statusCode: httpStatus.OK,
+//       success: true,
+//       message: 'Academic Semester created successfully',
+//       data: result,
+//     });
+//     next();
+//   },
+// );
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { ...academicSemesterData } = req.body;
-    const result = await AcademicSemesterService.createSemester(
-      academicSemesterData,
-    );
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Academic semester is created successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Academic Semester created successfully',
-      data: result,
-    });
-    next();
+
+    try {
+      const result = await AcademicSemesterService.createSemester(
+        academicSemesterData,
+      );
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Academic Semester created successfully',
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
   },
 );
 
+// const getAllSemesters = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const filters = pick(req.query, academicSemesterFilterableFields);
+//     const paginationOptions = pick(req.query, paginationFields);
+
+//     console.log(filters);
+
+//     // const paginationOptions = {
+//     //   page: Number(req.query.page),
+//     //   limit: Number(req.query.limit),
+//     //   sortBy: Number(req.query.sortBy),
+//     //   sortOrder: Number(req.query.sortOrder),
+//     // };
+
+//     console.log(paginationOptions);
+
+//     const result = await AcademicSemesterService.getAllSemesters(
+//       filters,
+//       paginationOptions,
+//     );
+//     sendResponse<IAcademicSemester[]>(res, {
+//       statusCode: httpStatus.OK,
+//       success: true,
+//       message: 'Semesters retrieved successfully',
+//       meta: result.meta,
+//       data: result.data,
+//     });
+//     next();
+//   },
+// );
+
 const getAllSemesters = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const filters = pick(req.query, ['searchTerm']);
+    const filters = pick(req.query, academicSemesterFilterableFields);
     const paginationOptions = pick(req.query, paginationFields);
 
-    console.log(paginationOptions);
+    // ...
 
     const result = await AcademicSemesterService.getAllSemesters(
       filters,
