a3af78230f225d330a23f5c635bfdbb74b60e48c
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-23 19:07:49 +0600

    searching work successfully

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -20,7 +20,7 @@ export type IAcademicSemesterCodes = '01' | '02' | '03';
 
 export type IAcademicSemester = {
   title: IAcademicSemesterTitles;
-  year: number;
+  year: string;
   code: IAcademicSemesterCodes;
   startMonth: IAcademicSemesterMonths;
   endMonth: IAcademicSemesterMonths;
