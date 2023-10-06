c08583e32c7e4eb56d93763ddc86953f1773c08c
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-10-07 04:30:16 +0600

    [DONE] student sevice and validation complete successfully

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import { IGenericResponse } from '../../../interfaces/common';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
@@ -5,6 +6,8 @@ import { SortOrder } from 'mongoose';
 import { IStudent, IStudentFilters } from './student.interface';
 import { studentSearchableFields } from './student.constant';
 import { Student } from './student.model';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
 
 const getAllStudents = async (
   filters: IStudentFilters,
@@ -81,7 +84,40 @@ const updateStudent = async (
   id: string,
   payload: Partial<IStudent>,
 ): Promise<IStudent | null> => {
-  const result = await Student.findOneAndUpdate({ _id: id }, payload, {
+  const isExist = Student.findOne({ id });
+  if (!isExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Student not found');
+  }
+
+  const { name, guardian, localGuardian, ...studentData } = payload;
+
+  const updatedStudentData: Partial<IStudent> = { ...studentData };
+
+  //dynamically handling
+  if (name && Object.keys(name).length > 0) {
+    Object.keys(name).forEach(key => {
+      const nameKey = `name.${key}`; //name.firstName
+      (updatedStudentData as any)[nameKey] = name[key as keyof typeof name];
+    });
+  }
+
+  if (guardian && Object.keys(guardian).length > 0) {
+    Object.keys(guardian).forEach(key => {
+      const guardianKey = `guardian.${key}`; //guardian.contactNo
+      (updatedStudentData as any)[guardianKey] =
+        guardian[key as keyof typeof guardian]; //updatedStudentData['guardian.contactNo']= guardian[contactNo];
+    });
+  }
+
+  if (localGuardian && Object.keys(localGuardian).length > 0) {
+    Object.keys(localGuardian).forEach(key => {
+      const localGuardianKey = `name.${key}`;
+      (updatedStudentData as any)[localGuardianKey] =
+        localGuardian[key as keyof typeof localGuardian];
+    });
+  }
+
+  const result = await Student.findOneAndUpdate({ id }, updatedStudentData, {
     new: true,
   });
   return result;
--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -19,20 +19,24 @@ const updateStudentZodSchema = z.object({
     academicSemester: z.string().optional(),
     academicDepartment: z.string().optional(),
     academicFaculty: z.string().optional(),
-    guardian: z.object({
-      fatherName: z.string().optional(),
-      fatherOccupation: z.string().optional(),
-      fatherContactNo: z.string().optional(),
-      motherName: z.string().optional(),
-      motherOccupation: z.string().optional(),
-      motherContactNo: z.string().optional(),
-    }),
-    localGuardian: z.object({
-      name: z.string().optional(),
-      occupation: z.string().optional(),
-      contactNo: z.string().optional(),
-      address: z.string().optional(),
-    }),
+    guardian: z
+      .object({
+        fatherName: z.string().optional(),
+        fatherOccupation: z.string().optional(),
+        fatherContactNo: z.string().optional(),
+        motherName: z.string().optional(),
+        motherOccupation: z.string().optional(),
+        motherContactNo: z.string().optional(),
+      })
+      .optional(),
+    localGuardian: z
+      .object({
+        name: z.string().optional(),
+        occupation: z.string().optional(),
+        contactNo: z.string().optional(),
+        address: z.string().optional(),
+      })
+      .optional(),
     profileImage: z.string().optional(),
   }),
 });
