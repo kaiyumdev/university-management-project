3d65d4b57eed8107641392a3a72da4c18872bd60
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-10-15 22:16:57 +0600

    [DONE] complete refreshToken handler successfully

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import { User } from '../users/user.model';
-import { ILoginUser, ILoginUserResponse } from './auth.interface';
+import {
+  ILoginUser,
+  ILoginUserResponse,
+  IRefreshTokenResponse,
+} from './auth.interface';
 // import jwt, { Secret } from 'jsonwebtoken';
 import config from '../../../config';
 import { jwtHelpers } from '../../../helpers/jwtHelpers';
+import { Secret } from 'jsonwebtoken';
 
 const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
   const { id, password } = payload;
@@ -54,8 +60,38 @@ const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
   return { accessToken, refreshToken, needsPasswordChange };
 };
 
+let verifyToken = null;
+const refreshToken = async (token: string): Promise<IRefreshTokenResponse> => {
+  try {
+    verifyToken = await jwtHelpers.verifyToken(
+      token,
+      config.jwt.refresh_token as Secret,
+    );
+    console.log(verifyToken);
+  } catch (error) {
+    throw new ApiError(httpStatus.FORBIDDEN, 'Invalid refresh token');
+  }
+  const { userId } = verifyToken;
+  //checking deleted user's refresh token
+  const isUserExist = await User.isUserExist(userId);
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist');
+  }
+
+  //generate new access token
+  const newAccessToken = jwtHelpers.createToken(
+    { id: isUserExist.id, role: isUserExist.role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string,
+  );
+  return {
+    accessToken: newAccessToken,
+  };
+};
+
 export const AuthService = {
   loginUser,
+  refreshToken,
 };
 
 //create access token & refresh token
